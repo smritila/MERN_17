1b30987ae7d60d952c7ff363a6974c46d1620d37
Smriti <smrtghoshal24@gmail.com>
2024-11-07 16:17:43 +0530

    chore: data transmission through child and props drill.

--- React js/project2/src/concepts/PropDrill1.jsx
+++ React js/project2/src/concepts/PropDrill1.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import PropDrill2 from "./PropDrill2";
 
 function PropDrill1(props) {
   console.log(props);
   return (
     <div>
       <h1> I am prop Drill 1 comp, displaying data from app</h1>
-      <p>My name is {props.student.name}</p>
-      <p>My age is {props.student.age}</p>
+      <p>I am busy, now cannot display data.</p>
+      {/* <p>My name is {props.student.name}</p>
+      <p>My age is {props.student.age}</p> */}
+      <PropDrill2 name={props.student.name} age={props.student.age} />
     </div>
   );
 }
